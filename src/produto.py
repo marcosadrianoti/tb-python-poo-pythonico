0d9d779a5f68b4615c6b8f3c515ba6af0049f2bd
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-08-22 18:26:48 -0300

    Req 1 done

--- src/produto.py
+++ src/produto.py
@@ -2,22 +2,28 @@ class Produto:
     def __init__(
         self, nome: str, codigo: str, preco: float, quantidade: int
     ) -> None:
-        self.nome = nome
-        self.codigo = codigo
-        self.preco = preco
-        self.quantidade = quantidade
+        self._Produto__nome = nome
+        self._Produto__codigo = codigo
+        self._Produto__preco = preco
+        self._Produto__quantidade = quantidade
 
     def get_preco(self) -> float:
-        pass
+        return self._Produto__preco
 
     def get_quantidade(self) -> int:
-        pass
+        return self._Produto__quantidade
 
     def atualizar_preco_do_produto(self, novo_preco: float) -> None:
-        pass
+        if novo_preco < 0:
+            print('preço precisa ser maior que zero')
+        else:
+            self._Produto__preco = novo_preco
 
     def adicionar_estoque_do_produto(self, quantidade: int) -> None:
-        pass
+        self._Produto__quantidade += quantidade
 
     def remover_estoque_do_produto(self, quantidade: int) -> None:
-        pass
+        if quantidade > self._Produto__quantidade:
+            raise ValueError('Estoque insuficiente para esta quantidade')
+        else:
+            self._Produto__quantidade -= quantidade
